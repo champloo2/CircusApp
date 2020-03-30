cea603df6f06248e3d6f1abfeae752753dec8903
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-03-29 20:30:11 -0500

    created new files ApparatusApplication, WebController, ApparatusRepository

---
+++ CircusApparatuses/src/main/java/dmacc/controller/WebController.java
@@ -0,0 +1,12 @@
+package dmacc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+	
+	@Autowired
+	ApparatusRepository repo;
+
+}
