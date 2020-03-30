09b0a14bb57352507000e3425542e17c56bdbe0f
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-03-29 20:28:20 -0500

    got to page 13 of spring beans intro contacts instructions

--- CircusApparatuses/src/main/java/dmacc/beans/Apparatus.java
+++ CircusApparatuses/src/main/java/dmacc/beans/Apparatus.java
@@ -2,17 +2,28 @@ package dmacc.beans;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Apparatus {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String nameOfApparatus;
 	private String level;
+	@Autowired  //do i need this?
 	private List<Maneuver> maneuverList;
 
 }
--- CircusApparatuses/src/main/java/dmacc/beans/Maneuver.java
+++ CircusApparatuses/src/main/java/dmacc/beans/Maneuver.java
@@ -1,15 +1,26 @@
 package dmacc.beans;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
 @NoArgsConstructor
+@Embeddable  //do I need this? 
+@Entity
 public class Maneuver {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String maneuverName;
 	private Apparatus apparatus;
+	
 
 }
