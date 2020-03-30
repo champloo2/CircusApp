bd3213804d9df51d0caa00ce63e761154b02a383
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-03-30 10:03:02 -0500

    wip

--- CircusApparatuses/src/main/java/dmacc/CircusApparatusesApplication.java
+++ CircusApparatuses/src/main/java/dmacc/CircusApparatusesApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CircusApparatusesApplication {
+public class CircusApparatusesApplication 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CircusApparatusesApplication.class, args);
